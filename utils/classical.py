310cf00ab7aaf28da8e3990c5cc9d5104c41488f
dylan <dylan.ebert@gmail.com>
2019-01-30 20:20:42 -0500

    .

--- utils/classical.py
+++ utils/classical.py
@@ -25,20 +25,47 @@ class Classical():
             n += 1
         return float(n_entails) / n
 
-    def entailment(self):
+    def entailment(self, save_path):
         wbless = WBless()
-        with open('results/classical.txt', 'w+') as f:
+        with open(save_path, 'w+') as f:
             for pair in wbless.pairs:
                 entails = self.entails(pair)
                 line = '{0}\t{1}\t{2}\t{3}'.format(get_word_from_label(pair[0]), get_word_from_label(pair[1]), pair[2], str(entails))
                 print(line)
                 f.write('{0}\n'.format(line))
 
+    def numerical_eval(self, save_path):
+        true = []
+        predicted = []
+        with open(save_path) as f:
+            for line in f:
+                _, _, t, p = line.rstrip().split('\t')
+                true.append(int(t))
+                predicted.append(float(p))
+        true_positives = 0
+        false_positives = 0
+        false_negatives = 0
+        for i in range(len(true)):
+            if true[i] == 1:
+                if predicted[i] == 1:
+                    true_positives += 1
+                else:
+                    false_negatives += 1
+            else:
+                if predicted[i] == 1:
+                    false_positives += 1
+        precision = true_positives / float(true_positives + false_positives)
+        recall = true_positives / float(true_positives + false_negatives)
+        f1 = 2 * (precision * recall) / (precision + recall)
+        print(precision, recall, f1)
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-m', '--model_path', type=str, required=True)
+    parser.add_argument('-m', '--model_path', type=str, default='model/vae2')
     parser.add_argument('--dbscan', action='store_true')
+    parser.add_argument('--save_path', type=str, default='results/CLASSICAL.txt')
     args = parser.parse_args()
 
     c = Classical(args.model_path, args.dbscan)
-    c.entailment()
+    c.entailment(args.save_path)
+    c.numerical_eval(args.save_path)
--- utils/dbscan.py
+++ utils/dbscan.py
@@ -1,14 +1,17 @@
 import numpy as np
 from sklearn.cluster import DBSCAN
+import random
 
-def dbscan_filter(encodings, eps=.05, min_samples=10):
+def dbscan_filter(encodings, eps=.05, min_samples=25):
+    if encodings.shape[0] > 100000:
+        encodings = np.array(random.sample(list(encodings), 100000))
     db = DBSCAN(eps=eps, min_samples=min_samples).fit(encodings)
     core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
     core_samples_mask[db.core_sample_indices_] = True
     labels = db.labels_
     unique_labels = set(labels)
     if len(unique_labels) == 1:
-        return dbscan_filter(encodings, eps=eps*2, min_samples=min_samples)
+        return dbscan_filter(encodings, eps=eps+.01, min_samples=min_samples)
     filtered = None
     max_members = 0
     for k in unique_labels:
